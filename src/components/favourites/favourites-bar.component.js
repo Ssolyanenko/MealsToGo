f810cdca8edd8307665706f3edc4310bb1b214f5
serhiisolianenko <cergejcolanenko@gmail.com>
2024-02-03 17:36:04 +0200

    add text for fav

--- src/components/favourites/favourites-bar.component.js
+++ src/components/favourites/favourites-bar.component.js
@@ -2,6 +2,7 @@ import React from "react";
 import {ScrollView, TouchableOpacity, View} from "react-native";
 import styled from "styled-components/native";
 import {CompactRestaurantInfo} from "../restaurant/compact-restaurant-info.component";
+import {Text} from "../typography/text.component";
 
 const FavouritesWrapper = styled.View`
     padding: 10px;
@@ -10,18 +11,19 @@ export const FavouritesBar = ({favorites, onNavigate}) => {
 
     return (
         <FavouritesWrapper>
-        <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-            {favorites.map((restaurant) => {
-                const key = restaurant.name.split(" ").join("")
-                return (<View key={key} style={{marginRight: 10}}>
-                    <TouchableOpacity onPress={() => onNavigate.navigate('RestaurantDetail', {
-                        restaurant
-                    })}>
-                        <CompactRestaurantInfo restourant={restaurant}/>
-                    </TouchableOpacity>
-                </View>)
-            })}
-        </ScrollView>
-    </FavouritesWrapper>
+            <Text variant='caption'> Favourites </Text>
+            <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+                {favorites.map((restaurant) => {
+                    const key = restaurant.name.split(" ").join("")
+                    return (<View key={key} style={{marginRight: 10}}>
+                        <TouchableOpacity onPress={() => onNavigate.navigate('RestaurantDetail', {
+                            restaurant
+                        })}>
+                            <CompactRestaurantInfo restourant={restaurant}/>
+                        </TouchableOpacity>
+                    </View>)
+                })}
+            </ScrollView>
+        </FavouritesWrapper>
     )
 }
