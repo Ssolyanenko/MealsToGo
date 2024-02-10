ed2462ebcce0c817645a2882a3b05112bfe25656
serhiisolianenko <cergejcolanenko@gmail.com>
2024-02-10 19:39:20 +0200

    add login.screen.js

--- src/features/account/screens/login.screen.js
+++ src/features/account/screens/login.screen.js
@@ -1,13 +1,33 @@
 import {AccountContainer, AccountCover, AccountStyled, AuthButton} from "../components/account.styled";
+import {useContext, useState} from "react";
+import {ActivityIndicator, TextInput} from "react-native-paper";
+import {AuthContext} from "../../../services/auth/auth.context";
 
 export const LoginScreen = () =>{
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const {onLogin,isLoading} = useContext(AuthContext)
+if(isLoading) return <ActivityIndicator/>
     return(
         <AccountStyled>
             <AccountCover/>
             <AccountContainer>
+                <TextInput
+                    label="Email"
+                    value={email}
+                    onChangeText={text => setEmail(text)}
+                    style={{marginBottom:10, width:300}}
+                />
+                <TextInput
+                    label="Password"
+                    value={password}
+                    onChangeText={text => setPassword(text)}
+                    style={{marginBottom:10, width:300}}
+                />
                 <AuthButton
                     icon='lock-open-outline'
                     mode='contained'
+                    onPress={()=>onLogin(email, password)}
                 >Login</AuthButton>
             </AccountContainer>
         </AccountStyled>
--- src/services/auth/auth.context.js
+++ src/services/auth/auth.context.js
@@ -8,17 +8,21 @@ export const AuthContextProvider = ({children}) => {
     const [error, setError] = useState(null)
 
     const onLogin = async (email, password) => {
+        console.log(email,password)
         setIsLoading(true)
         await loginRequest(email, password).then((u) => {
             setUser(u)
             setIsLoading(false)
+            console.log(u)
         }).catch((error) => {
             setIsLoading(false)
+            console.log(error)
             setError(error)
         })
     }
     return (
         <AuthContext.Provider value={{
+            isAuth: !!user,
             user,
             isLoading,
             error,
