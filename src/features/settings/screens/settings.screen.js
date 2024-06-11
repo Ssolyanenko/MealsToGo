31e4c29c2064065607a1ad2e5763461c71dabf59
Ssolyanenko <cergejcolanenko@email.com>
2024-06-11 10:17:41 +0300

    clean up settings.screen.js

--- src/features/settings/screens/settings.screen.js
+++ src/features/settings/screens/settings.screen.js
@@ -1,29 +1,46 @@
 import React, {useContext} from "react";
+import {Text} from "react-native";
 import {AuthContext} from "../../../services/auth/auth.context";
 import {SafeArea} from "../../../components/utility/safe-area.component";
-import {List} from "react-native-paper";
+import {List, Avatar} from "react-native-paper";
+import styled from "styled-components/native";
+import {SpacerComponent} from "../../../components/spacer/spacer.component";
 
+const SettingsItem = styled(List.Item)`
+    padding: ${props => props.theme.space[3]};`
+
+const AvatarContainer = styled.View`
+    align-items: center;
+`
 export const SettingsScreen = () => {
-    const {onLogOut} = useContext(AuthContext)
+    const {onLogOut,user} = useContext(AuthContext)
     return (
         <SafeArea>
-           <List.Section>
-               <List.Section>
-                   <List.Item
-                       style={{ padding: 16 }}
-                       title="Favourites"
-                       description="View your favourites"
-                       left={(props) => <List.Icon {...props} color="black" icon="heart" />}
-                       onPress={() => navigation.navigate("Favourites")}
-                   />
-                   <List.Item
-                       style={{ padding: 16 }}
-                       title="Logout"
-                       left={(props) => <List.Icon {...props} color="black" icon="door" />}
-                       onPress={onLogOut}
-                   />
-               </List.Section>
-           </List.Section>
+            <AvatarContainer>
+                <Avatar.Icon size={180} icon="human" backgroundColor="#2182BD"/>
+            </AvatarContainer>
+            <SpacerComponent position='top' size='large' marginTop={20}>
+           <Text variant="label" alignSelf='center'>
+               {user.email}
+           </Text>
+            </SpacerComponent>
+            <List.Section>
+                <List.Section>
+                    <SettingsItem
+                        style={{padding: 16}}
+                        title="Favourites"
+                        description="View your favourites"
+                        left={(props) => <List.Icon {...props} color="black" icon="heart"/>}
+                        onPress={() => navigation.navigate("Favourites")}
+                    />
+                    <SettingsItem
+                        style={{padding: 16}}
+                        title="Logout"
+                        left={(props) => <List.Icon {...props} color="black" icon="door"/>}
+                        onPress={onLogOut}
+                    />
+                </List.Section>
+            </List.Section>
         </SafeArea>
     )
 };
