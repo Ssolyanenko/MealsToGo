baf49187f57885da3d1ad199f9e7d82d1e59e580
Serhii <cergejcolanenko@gmail.com>
2024-12-31 16:51:21 +0200

    camera improovments

--- src/features/settings/screens/camera.screen.js
+++ src/features/settings/screens/camera.screen.js
@@ -1,35 +1,43 @@
-import React, { useRef, useState, useEffect } from "react";
-import { Camera } from "expo-camera";
+import React, {useRef, useState, useEffect} from "react";
+import {Camera} from "expo-camera";
 import styled from "styled-components/native";
-import { View } from "react-native";
-import { Text } from "../../../components/typography/text.component";
+import {TouchableOpacity, View} from "react-native";
+import {Text} from "../../../components/typography/text.component";
 
 const ProfileCamera = styled(Camera)`
-  width: 100%;
-  height: 100%;
+    width: 100%;
+    height: 100%;
 `;
 
 export const CameraScreen = () => {
     const [hasPermission, setHasPermission] = useState(null);
     const cameraRef = useRef();
-
+    const snap = async () => {
+        if (cameraRef) {
+            const photo = await cameraRef.current.takePictureAsync()
+            console.log(photo)
+        }
+    }
     useEffect(() => {
         (async () => {
-            const { status } = await Camera.requestPermissionsAsync();
+            const {status} = await Camera.requestCameraPermissionsAsync();
             setHasPermission(status === "granted");
         })();
     }, []);
 
     if (hasPermission === null) {
-        return <View />;
+        return <View/>;
     }
     if (hasPermission === false) {
         return <Text>No access to camera</Text>;
     }
     return (
-        <ProfileCamera
-            ref={(camera) => (cameraRef.current = camera)}
-            type={Camera.Constants.Type.front}
-        ></ProfileCamera>
+        <TouchableOpacity onPress={snap}>
+            <ProfileCamera
+                ref={(camera) => (cameraRef.current = camera)}
+                type={Camera.Constants.Type.front}
+            >
+            </ProfileCamera>
+        </TouchableOpacity>
     );
 };
